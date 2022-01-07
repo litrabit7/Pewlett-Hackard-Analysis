7b8a1912386d2ffc68c6c05401e7c4c458a9af07
litrabit7 <88869506+litrabit7@users.noreply.github.com>
2022-01-07 03:46:23 -0800

    add mentorship_eligibilty csv and update sql queries

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,5 @@
+--retirement_titles
+
 -- create eligible retirement employee table with job title 
 select e.emp_no,
 	e.first_name,
@@ -12,6 +14,8 @@ on (e.emp_no = t.emp_no)
 where (e.birth_date between '1952-01-01' and '1955-12-31')
 order by e.emp_no;
 
+--unique_titles
+
 --retrieve unique employee record with latest title 
 -- Use Dictinct with Orderby to remove duplicate rows
 select distinct on (emp_no) emp_no, 
@@ -22,8 +26,11 @@ into unique_titles
 from retirement_titles 
 order by emp_no, to_date desc; 
 
+
 --count # of unique job titles from unique title table
 select count (distinct title) from unique_titles;
+
+--retiring_titles
 
 --retrieve # of employee retireing from each title 
 -- in decending order by total
@@ -32,3 +39,24 @@ into retiring_titles
 from unique_titles
 group by title
 order by count (emp_no) desc;
+
+--mentorship eligibility 
+
+--retrieve employee elegible for the mentor program 
+-- latest department, --birthdate in 1965 
+-- order by emp # 
+select distinct on (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+into mentorship_eligibilty
+from employees as e
+inner join dept_emp as de
+on (e.emp_no = de.emp_no)
+inner join titles as t
+on (e.emp_no = t.emp_no)
+where (e.birth_date between '1965-01-01' and '1965-12-31')
+order by e.emp_no, de.to_date desc;
