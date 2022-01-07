f1e24bd8c95627882085dc5ac9a3a2be1daa862f
litrabit7 <88869506+litrabit7@users.noreply.github.com>
2022-01-06 23:22:13 -0800

    change to Schema SQL queries

--- Schema.sql
+++ Schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE employees (
 	 hire_date DATE NOT NULL,
      PRIMARY KEY (emp_no)
     );
-CREATE TABLE dept_manager (
+CREATE TABLE manager (
 	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
@@ -34,4 +34,30 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no varchar NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title varchar NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+drop table titles cascade;
+
 SELECT * FROM departments;
+SELECT * FROM employees;
+select * from manager;
+select * from salaries;
+select * from dept_emp;
+select * from titles;
+
+ 
